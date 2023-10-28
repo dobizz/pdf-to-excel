ea1125adc4ad030799ea4c6bd179518346da537c
Ronnie Villanueva <ronnie.code@outlook.com>
2023-10-28 10:42:03 +0800

    refactor(main.py): move process_pdf to pdf2excel.tasks

--- main.py
+++ main.py
@@ -1,79 +1,8 @@
 import glob
-import re
 import time
 from pathlib import Path
 
-import pandas as pd
-import pdfplumber
-
-from pdf2excel.regexes import core_pat, entry_pat, head_pat
-
-
-def process_pdf(path: Path):
-    print(f"Processing file {path}")
-
-    # extract lines
-    with pdfplumber.open(path) as pdf:
-        all_lines = []
-        for page in pdf.pages:
-            text = page.extract_text(keep_blank_chars=True)
-            core = re.search(core_pat, text)
-            if not core:
-                continue
-            core = core.group(1)
-            lines = core.split("\n")
-            all_lines.extend(lines)
-
-    # build groups
-    heads = []
-    entries = []
-    for line in all_lines:
-        head_search = head_pat.search(line)
-        entry_search = entry_pat.search(line)
-        if head_search:
-            head = head_search.groupdict()
-            heads.append(head)
-        if entry_search:
-            entry = entry_search.groupdict()
-            entries.append(entry)
-
-    # write output
-    df1 = pd.DataFrame(heads)
-    df2 = pd.DataFrame(entries)
-    df1.to_excel(
-        path.stem + "_extraction_1_.xlsx",
-        index=False,
-        header=[
-            "Division",
-            "SV_Item",
-            "UPC",
-            "Desc",
-            "Pack",
-            "Size",
-            "Example",
-        ],
-    )
-    df2.to_excel(
-        path.stem + "_extraction_2_.xlsx",
-        index=False,
-        header=[
-            "PO_Nbr",
-            "PO_Date",
-            "Inv_Nbr",
-            "Inv_Date",
-            "Rcv_Date",
-            "Rcv_Wgt",
-            "Paid_Net",
-            "PO_Net",
-            "Paid_List",
-            "Frt_Allow",
-            "PO_List",
-            "PO_Paid_Variance",
-            "Rcv_Qty",
-            "Prior_Billed",
-            "Claim",
-        ],
-    )
+from pdf2excel.tasks import process_pdf
 
 
 def main():
