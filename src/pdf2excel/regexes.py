130d3cbd41bae970930ad11752094454b17220fe
Ronnie Villanueva <ronnie.code@outlook.com>
2023-10-30 03:36:24 +0800

    feat: add extraction of invoice page

--- src/pdf2excel/regexes.py
+++ src/pdf2excel/regexes.py
@@ -32,3 +32,16 @@ entry_pat = re.compile(
     r"(?P<prior_billed>[0-9.,]*)\s*?"
     r"(?P<claim>[0-9.,]+)\s*"
 )
+
+invoice_page_pat = re.compile(
+    r"BILL TO:\s*(?P<bill_to>\w+)\s*"
+    r"SV Barcode:\s*(?P<sv_barcode>\w+)\s*"
+    r"(?P<vendor_name>.*)\s* Audit Period:\s*(?P<audit_period>[0-9-]+)\s*"
+    r"(?P<address_1>.*)\s*Claim:\s*(?P<claim_no>\w+)\s*"
+    r"(?P<address_2>.*)\s*Date:\s(?P<claim_date>\d{1,2}/\d{1,2}/\d{2,4})\s*"
+    r"Claim Amount\s*\$\s*(?P<claim_amount>[0-9.,]+)\s*"
+    r"Claim Code\s*(?P<claim_code>[a-zA-Z0-9.]+)\s*"
+    r"Description\s*(?P<description>.*)\s*"
+    r"Region\s*(?P<region>.*)\s*"
+    r"Category\s*(?P<category>.*)"
+)
