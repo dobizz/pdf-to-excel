f7813b7bce0c4652c08a58201ed73c9819773f13
Ronnie Villanueva <ronnie.code@outlook.com>
2023-12-09 11:24:40 +0800

    refactor: move tasks to process_1

--- app.py
+++ app.py
@@ -3,14 +3,11 @@ import logging
 import sys
 import time
 import tkinter as tk
-from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from tkinter import filedialog
 
-import pandas as pd
-
 from pdf2excel import __version__
-from pdf2excel.tasks import process_pdf
+from pdf2excel.tasks import process_1
 
 
 def main(directory: str):
@@ -27,45 +24,8 @@ def main(directory: str):
         # prompt user to continue as files might be too many
         input("\n[Press ENTER to continue]\n")
 
-        # run tasks in parallel using max of 8 threads
         t0 = time.perf_counter()
-        with ThreadPoolExecutor(max_workers=8) as pool:
-            results = pool.map(process_pdf, files)
-        invoice_data = list(results)
-        # generate output for invoices
-        df = pd.DataFrame(invoice_data)
-        df.to_excel(
-            "invoice_summary.xlsx",
-            index=False,
-            columns=[
-                "bill_to",
-                "vendor_name",
-                "address",
-                "sv_barcode",
-                "audit_period",
-                "claim_no",
-                "claim_date",
-                "claim_amount",
-                "claim_code",
-                "description",
-                "region",
-                "category",
-            ],
-            header=[
-                "Bill_To",
-                "Vendor_Name",
-                "Address",
-                "SV_Barcode",
-                "Audit_Period",
-                "Claim_No",
-                "Claim_Date",
-                "Claim_Amount",
-                "Claim_Code",
-                "Description",
-                "Region",
-                "Category",
-            ],
-        )
+        process_1(files=files)
         t1 = time.perf_counter()
 
         # show metrics
